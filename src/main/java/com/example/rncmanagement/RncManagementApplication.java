8209d7e069309715f8b25e86f92ef3892236d7bf
augustocz <augusto.capmany@gmail.com>
2024-03-21 12:08:59 -0300

    first commit

---
+++ src/main/java/com/example/rncmanagement/RncManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.rncmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RncManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RncManagementApplication.class, args);
+    }
+
+}
